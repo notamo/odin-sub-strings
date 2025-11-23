4b08b9bde3603e3740a5fd9a0b39e32b983b5d78
notamo <203415281+notamo@users.noreply.github.com>
2025-11-23 20:30:51 +0100

    Replace String#index with String#scan method

--- substrings.rb
+++ substrings.rb
@@ -1,15 +1,15 @@
-def substrings(string, substring_array)
-  substring_array.reduce(Hash.new(0)) do |acc, word|
-    offset = 0
-    while offset = string.downcase.index(word, offset)
-        acc[word] += 1
-        offset += 1
-    end
+def substrings(string, dictionary)
+  dictionary.reduce(Hash.new(0)) do |acc, word|
+    count = string.downcase.scan(word).size
+    acc[word] = count if count > 0
     acc
   end
 end
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = [
+  "below", "down", "go", "going", "horn", "how", "howdy",
+  "it", "i", "low", "own", "part", "partner", "sit"
+]
 
 puts substrings("below", dictionary)
 puts substrings("Howdy partner, sit down! How's it going?", dictionary)
